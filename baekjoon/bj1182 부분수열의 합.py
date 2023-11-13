cdcdf25e2d92bfcd84d31500ef4d32d9ecda1e22
mochacina <nayuta5554@gmail.com>
2023-11-13 17:51:13 +0900

    문제풀이 반영

--- baekjoon/bj1182 부분수열의 합.py
+++ baekjoon/bj1182 부분수열의 합.py
@@ -1,27 +1,42 @@
-n, s = map(int,input().split())
-l = list(map(int,input().split()))
+# n, s = map(int,input().split())
+# l = list(map(int,input().split()))
 
-list = []
-answer = []
+# list = []
+# ans = []
 
-def dfs(a):
-    if len(list) != 0 and sum(l[i] for i in list) == s:
-        #print(list)
-        if set(list) not in answer:
-            answer.append(set(list))
-        return
-    for i in range(a,n):
-        if i not in list:
-            list.append(i)
-            dfs(a+1)
-            list.pop()
+# def dfs(a,b):
+#     if len(list) == b and sum(list) == s:
+#         if(set(list) not in ans): ans.append(set(list))
+#         print(list)
+#         return
+#     for i in range(a,n):
+#         if l[i] not in list:
+#             list.append(l[i])
+#             dfs(a+1,b)
+#             list.pop()
 
-dfs(0)
-print(len(answer))
-
+# for j in range(1,n+1):dfs(0,j)
+# print(len(ans))
     
 # for i in range(n): # 숫자 몇개 고를지 for문
 #     buf = []
 #     for _ in range(i):
 #         for j in range(n):
-            
+
+
+def solve(idx, SUM):
+    print(idx, SUM)
+    global ans
+    if idx >= N:
+        return
+    SUM += nums[idx]
+    if SUM == S:
+        ans += 1
+    solve(idx + 1, SUM - nums[idx])
+    solve(idx + 1, SUM)
+
+N, S = map(int, input().split())
+nums = list(map(int, input().split()))
+ans = 0
+solve(0, 0)
+print(ans)
