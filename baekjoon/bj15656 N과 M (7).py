60e7624cf2d46851df4429a43dca5674d2209169
mochacina <nayuta5554@gmail.com>
2023-11-14 17:52:17 +0900

    문제풀이 반영

--- baekjoon/bj15656 N과 M (7).py
+++ baekjoon/bj15656 N과 M (7).py
@@ -1,5 +1,6 @@
 n,m=map(int,input().split())
+l=sorted(list(map(int,input().split())))
 def f(a,b=[]):
  if a==m:print(*b);return
- [f(a+1,b+[sorted(list(map(int,input().split())))[i]])for i in range(n)]
+ [f(a+1,b+[l[i]])for i in range(n)]
 f(0)
