8530a95b48e7d4cc485e8d4f5381d92cf17a269a
DESKTOP-AMLBOG5\Soh <nayuta5554@gmail.com>
2023-11-19 04:24:44 +0900

    문제풀이 반영

--- baekjoon/bj18111 마인크래프트.py
+++ baekjoon/bj18111 마인크래프트.py
@@ -1,6 +1,23 @@
-n,m,b = map(int,input().split())
-l = [list(map(int,input().split())) for _ in range(n)]
-time = 0
-while 1:
-    max_l = max([max(i) for i in l])
-    sum_l = sum([sum(i) for i in l])
+import sys
+N,M,B = map(int,input().split())
+l = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N)]
+answer = sys.maxsize
+idx = 0
+
+for floor in range(257):
+    pb, mb = 0, 0
+
+    for i in range(N):
+        for j in range(M):
+            if l[i][j] > floor :
+                pb += l[i][j] - floor
+            else : 
+                mb += floor - l[i][j]
+
+    if pb + B >= mb :
+        if (pb * 2) + mb <= answer:
+            answer = (pb * 2) + mb
+            idx = floor
+
+
+print(answer, idx)
