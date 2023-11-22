3f6e6cd9d53305de38e86e496063fcd064dff437
DESKTOP-AMLBOG5\Soh <nayuta5554@gmail.com>
2023-11-23 02:32:37 +0900

    문제풀이 반영

--- baekjoon/bj15664 N과 M(10).py
+++ baekjoon/bj15664 N과 M(10).py
@@ -1,16 +1,35 @@
+# n,m = map(int, input().split())
+# l = sorted(list(map(int,input().split())))
+# l2=[]
+# v=[0]*n
+# def dfs(s=[]):
+#     if len(s) == m:
+#         if set(s) not in l2:
+#             l2.append(set(s))
+#             print(' '.join(map(str,s)))
+#         return
+#     for i in range(n):
+#         if not v[i]:
+#             v[i]=1
+#             dfs(s+[l[i]])
+#             v[i]=0
+# dfs()
+
+
 n,m = map(int, input().split())
 l = sorted(list(map(int,input().split())))
 l2=[]
 v=[0]*n
-def dfs(s=[]):
+def dfs(a,s=[]):
     if len(s) == m:
-        if set(s) not in l2:
-            l2.append(set(s))
-            print(' '.join(map(str,s)))
+        t = ' '.join(map(str,s))
+        if t not in l2:
+            l2.append(t)
         return
-    for i in range(n):
+    for i in range(a,n):
         if not v[i]:
             v[i]=1
-            dfs(s+[l[i]])
+            dfs(i+1,s+[l[i]])
             v[i]=0
-dfs()
+dfs(0)
+[print(i)for i in l2]
---
+++ baekjoon/bj15666 N과 M (12).py
@@ -0,0 +1,14 @@
+n,m = map(int, input().split())
+l = sorted(list(map(int,input().split())))
+l2=[]
+def dfs(s=[]):
+    if len(s) == m:
+        print(*s)
+        return
+    o = 0
+    for i in range(n):
+        if o != l[i]:
+            o = l[i]
+            if not len(s) or s[-1] <= l[i]: 
+                dfs(s+[l[i]])
+dfs()
