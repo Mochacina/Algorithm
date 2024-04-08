e691c456867c032880dd2830e915aba0b77f7e91
Mochacina <nayuta5554@gmail.com>
2024-04-09 02:59:06 +0900

    commit

--- baekjoon/bj15961 회전 초밥.py
+++ baekjoon/bj15961 회전 초밥.py
@@ -1,5 +1,25 @@
 N, d, k, c = map(int, input().split())
-arr = []
+sushis = [int(input()) for _ in range(N)]
+l = [0] * (d+1)
+l[c] = 1
+ate = 1
 
-for _ in range(N):
-    arr.append(int(input()))
+for i in range(k):
+    sushi = sushis[i%N]
+    if not l[sushi]: ate += 1
+    l[sushi] += 1
+    
+max_n = ate
+
+for i in range(N):
+    out_sushi = sushis[i%N]
+    in_sushi = sushis[(i+k) % N]
+
+    l[out_sushi] -= 1
+    if not l[in_sushi]: ate += 1
+    if not l[out_sushi]: ate -= 1  
+    l[in_sushi] += 1
+    
+    max_n = max(ate, max_n)
+
+print(max_n)
