7e1f0a3bf998143cdafddc87c6cab2ab2c910b91
Mochacina <nayuta5554@gmail.com>
2024-04-17 02:34:36 +0900

    commit solved

--- baekjoon/bj1717 집합의 표현.py
+++ baekjoon/bj1717 집합의 표현.py
@@ -15,16 +15,17 @@
 #     if n: print(['NO','YES'][find(a)==find(b)])
 #     else: union(a,b)
 
+import sys
 n,m = map(int,input().split())
 li = [i for i in range(n+1)]
-def finder(a):
+def f(a):
     if a!=li[a]:
-        li[a] = finder(li[a])
+        li[a] = f(li[a])
     return li[a]
 for _ in range(m):
-    n,a,b = map(int,input().split())
-    if n: print(['NO','YES'][finder(a)==finder(b)])
+    n,a,b = map(int, sys.stdin.readline().split())
+    if n: print(['NO','YES'][f(a)==f(b)])
     else:
-        l = finder(a)
-        r = finder(b)
+        l = f(a)
+        r = f(b)
         li[r] = l
