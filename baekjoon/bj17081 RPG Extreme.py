577de863dbfb52526a4c40e132095f373e0aa66f
mochacina <nayuta5554@gmail.com>
2024-04-18 18:37:06 +0900

    commit

--- baekjoon/bj17081 RPG Extreme.py
+++ baekjoon/bj17081 RPG Extreme.py
@@ -9,14 +9,25 @@ class Hero:
         self.weapon = None
         self.armor = None
         self.accessories = []
-        self.position = [0, 0]
+        self.position = tuple([0, 0])  # Fix: Pass the position as a single argument to the tuple() function
+        self.terrain = '.'
         
     def show(self):
         print(f'LV : {self.level}')
         print(f'HP : {self.hp}/{self.max_hp}')
-        print(f'ATT : {self.base_attack}+{self.weapon.attack if self.weapon else 0}')
-        print(f'DEF : {self.base_defense}+{self.armor.defense if self.armor else 0}')
+        print(f'ATT : {self.base_attack}+{self.weapon if self.weapon else 0}')
+        print(f'DEF : {self.base_defense}+{self.armor if self.armor else 0}')
         print(f'EXP : {self.exp}/{self.level*5}')
+        
+    def get_exp(self, exp):
+        self.exp += exp
+        while self.exp >= self.level*5:
+            self.exp = 0
+            self.level += 1
+            self.max_hp += 5
+            self.hp = self.max_hp
+            self.base_attack += 2
+            self.base_defense += 2
         
 class Monster:
     def __init__(self, name, attack, defense, max_hp, exp, position):
@@ -26,7 +37,7 @@ class Monster:
         self.hp = max_hp
         self.max_hp = max_hp
         self.exp = exp
-        self.position = position
+        self.position = tuple(position)
         self.is_boss = False
         
 class ItemBox:
@@ -42,28 +53,6 @@ class GameMap:
         self.monsters = monsters
         self.item_boxes = item_boxes
         
-N,M = map(int, input().split())
-grid = [list(input()) for _ in range(N)]
-hero = Hero()
-monsters = []
-item_boxes = []
-for i in range(N):
-    for j in range(M):
-        if grid[i][j] == '@':
-            hero.position = [i, j]
-hero_moves = input()
-while 1:
-    try:
-        x, y, *l = input().split()
-        if len(l) == 5:
-            monsters.append(Monster(l[0], int(l[1]), int(l[2]), int(l[3]), int(l[4]), [int(x), int(y)]))
-        elif len(l) == 2:
-            item_boxes.append(ItemBox(l[0], l[1], [int(x), int(y)]))
-    except: break
-hero.show()
-print(monsters)
-print(item_boxes)
-game_map = GameMap(grid, hero, monsters, item_boxes)
 def simulate_combat(hero, monster):
     while True:
         # Hero attacks first
@@ -80,9 +69,9 @@ def simulate_combat(hero, monster):
 
 def process_item_box(hero, item_box):
     if item_box.item_type == 'W':
-        hero.weapon = item_box.effect
+        hero.weapon = int(item_box.effect)
     elif item_box.item_type == 'A':
-        hero.armor = item_box.effect
+        hero.armor = int(item_box.effect)
     elif item_box.item_type == 'O':
         if len(hero.accessories) < 4 and item_box.effect not in hero.accessories:
             hero.accessories.append(item_box.effect)
@@ -105,36 +94,71 @@ def move_hero(game_map, moves):
         new_position = (hero.position[0] + dx, hero.position[1] + dy)
         
         if not in_bounds(new_position) or grid[new_position[0]][new_position[1]] == '#':
-            continue  # Skip move if out of bounds or hits a wall
-        
-        hero.position = new_position
-        
-        # Check for item box at the new position
-        if hero.position in item_boxes:
-            item_box = item_boxes.pop(hero.position)
-            process_item_box(hero, item_box)
-            grid[hero.position[0]][hero.position[1]] = '.'
-        
-        # Check for monster at the new position
-        if hero.position in monsters_dict:
-            monster = monsters_dict[hero.position]
-            combat_result = simulate_combat(hero, monster)
-            if not combat_result:  # Hero defeated
-                hero_killed_by = monster.name
-                return turn_count, f"YOU HAVE BEEN KILLED BY {hero_killed_by}"
-            else:
-                monsters_dict.pop(hero.position)  # Remove defeated monster
+            pass
+        else: 
+            hero.position = new_position
+            
+            # Check for item box at the new position
+            if hero.position in item_boxes:
+                item_box = item_boxes.pop(hero.position)
+                process_item_box(hero, item_box)
                 grid[hero.position[0]][hero.position[1]] = '.'
+            
+            # Check for monster at the new position
+            if hero.position in monsters_dict:
+                monster = monsters_dict[hero.position]
+                combat_result = simulate_combat(hero, monster)
+                if not combat_result:  # Hero defeated
+                    hero_killed_by = monster.name
+                    hero.hp = 0
+                    return turn_count, f"YOU HAVE BEEN KILLED BY {hero_killed_by}"
+                else:
+                    hero.get_exp(monster.exp)
+                    monsters_dict.pop(hero.position)  # Remove defeated monster
+                    grid[hero.position[0]][hero.position[1]] = '.'
         
         # Check for spike trap
         if grid[hero.position[0]][hero.position[1]] == '^':
             hero.hp -= 5  # Damage from spike trap
             if hero.hp <= 0:
                 hero_killed_by = "SPIKE TRAP"
+                hero.hp = 0
                 return turn_count, f"YOU HAVE BEEN KILLED BY {hero_killed_by}"
 
-        grid[hero.position[0]][hero.position[1]] = '@'  # Move hero to new position
+        #grid[hero.position[0]][hero.position[1]] = '@'  # Move hero to new position
+        
+        print(f"Turns: {turn_count}")
+        #for i in grid: print(''.join(i))
+        for i in range(len(grid)):
+            if i == hero.position[0]:
+                print(''.join(grid[i][:hero.position[1]]+['@']+grid[i][hero.position[1]+1:]))
+            else:
+                print(''.join(grid[i]))
 
     return turn_count, "Press any key to continue."
+        
+N,M = map(int, input().split())
+grid = [list(input()) for _ in range(N)]
+hero = Hero()
+monsters = []
+item_boxes = []
+for i in range(N):
+    for j in range(M):
+        if grid[i][j] == '@':
+            hero.position = tuple([i, j])
+hero_moves = input()
+while 1:
+    try:
+        x, y, *l = input().split()
+        if len(l) == 5:
+            monsters.append(Monster(l[0], int(l[1]), int(l[2]), int(l[3]), int(l[4]), tuple([int(x), int(y)])))
+        elif len(l) == 2:
+            item_boxes.append(ItemBox(l[0], l[1], tuple([int(x), int(y)])))
+    except: break
+hero.show()
+game_map = GameMap(grid, hero, monsters, item_boxes)
 
-move_hero(game_map, hero_moves)
+turn_count, txt = move_hero(game_map, hero_moves)
+print(f"Turns: {turn_count}")
+hero.show()
+print(txt)
