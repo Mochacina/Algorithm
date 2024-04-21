efb8e242fc2568b5688b1106c70178290b133045
Mochacina <nayuta5554@gmail.com>
2024-04-21 23:56:36 +0900

    commit

---
+++ baekjoon/bj11723 집합.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+
+num = 0
+for _ in range(M):
+    cmd = input().split()
+    if len(cmd) == 1:
+        if cmd[0] == 'all':
+            num = (1 << 21) - 1
+        elif cmd[0] == 'empty':
+            num = 0
+    else:
+        k = int(cmd[1])
+        idx = 1 << (k - 1)
+        if cmd[0] == 'add':
+            num |= idx            
+        elif cmd[0] == 'remove':
+            num &= ~idx
+        elif cmd[0] == 'toggle':
+            num ^= idx
+        elif cmd[0] == 'check':
+            print(1 if num & idx else 0)
