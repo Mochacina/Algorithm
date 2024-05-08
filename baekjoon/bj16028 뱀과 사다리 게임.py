ee9c9db7392a27ac7b0dcceff8cf5801cf8747ee
Mochacina <nayuta5554@gmail.com>
2024-05-09 05:32:19 +0900

    commit solved

--- baekjoon/bj16028 뱀과 사다리 게임.py
+++ baekjoon/bj16028 뱀과 사다리 게임.py
@@ -1,22 +1,17 @@
 from collections import deque
-
 n,m = map(int, input().split())
 l = [0]*101
 v = [0]*101
 for _ in range(n+m):
     x,y = map(int, input().split())
     l[x] = y
-
 q = deque([(1,0)])
 while q:
     node, cnt = q.popleft()
-    if node == 100: break
-    for i in [1,2,3,4,5,6]:
-        fwd = node+i
-        if fwd > 100: break
-        if l[fwd]: fwd = l[fwd]
-        if not v[fwd]:
-            v[fwd] = cnt+1
-            q.append((fwd, cnt+1))
-            
-print(v[100])
+    for i in range(6):
+        f = node+i+1
+        if f > 99: exit(print(cnt+1))
+        if l[f]: f = l[f]
+        if not v[f]:
+            v[f] = cnt+1
+            q.append((f, cnt+1))
