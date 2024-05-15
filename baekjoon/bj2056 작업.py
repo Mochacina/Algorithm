4f963d253796f4f7bb175956de29793afb147057
Mochacina <nayuta5554@gmail.com>
2024-05-16 03:13:57 +0900

    commit solved

---
+++ baekjoon/bj2056 작업.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n = int(input())
+g = [[] for _ in range(n+1)]
+d = [0]*(n+1)
+t = [0]*(n+1)
+
+for i in range(1,n+1):
+    a,b,*l = map(int,input().split())
+    t[i] = a
+    for j in l:
+        g[j].append(i)
+        d[i] += 1
+
+q = deque()
+time = [0]*(n+1)
+for i in range(1, n+1):
+    if not d[i]:
+        q.append(i)
+        time[i] = t[i]
+
+while q:
+    node = q.popleft()
+    
+    for i in g[node]:
+        d[i] -= 1
+        time[i] = max(time[node]+t[i], time[i])
+        if d[i] == 0:
+            q.append(i)
+
+print(max(time))
