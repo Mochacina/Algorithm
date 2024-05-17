c1f8b8593427c8e738c0f1142b3d5ad613bbc533
mochacina <nayuta5554@gmail.com>
2024-05-17 13:43:19 +0900

    commit solved

--- baekjoon/bj4485 녹색 옷 입은 애가 젤다지.py
+++ baekjoon/bj4485 녹색 옷 입은 애가 젤다지.py
@@ -8,20 +8,19 @@ while n := int(input()):
     l = [[*map(int, input().split())] for _ in range(n)]
     cost_map = [[float("INF")]*n for _ in range(n)]
     
-    q = [(l[0][0],(0,0))]
+    q = [(0,0,0)]
     cost_map[0][0] = l[0][0]
     while q:
-        co, node = heapq.heappop(q)
-        x,y = node
+        co,x,y = heapq.heappop(q)
         
         if co > cost_map[x][y]: continue
         for dx, dy in di:
             nx, ny = x+dx, y+dy
             if 0 <= nx < n and 0 <= ny < n:
-                dcost = l[nx][ny]
-                cost = dcost + co
+                next_cost = l[nx][ny]
+                cost = next_cost + cost_map[x][y]
                 if cost < cost_map[nx][ny]:
                     cost_map[nx][ny] = cost
-                    heapq.heappush(q, (cost,(nx,ny)))
+                    heapq.heappush(q, (cost,nx,ny))
 
     print(f"Problem {p}: {cost_map[-1][-1]}")
