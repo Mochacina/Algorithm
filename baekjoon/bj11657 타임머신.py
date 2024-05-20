732332940b94a0e91220c75806555f9ecd03bf01
mochacina <nayuta5554@gmail.com>
2024-05-20 17:03:46 +0900

    commit solved

--- baekjoon/bj11657 타임머신.py
+++ baekjoon/bj11657 타임머신.py
@@ -7,3 +7,18 @@ for _ in range(m):
 distances = [float("INF")]*(n+1)
 distances[1] = 0
 
+for _ in range(n-1):
+    for i in range(1,n+1):
+        for city, w in cities[i]:
+            if distances[i] == float("INF"): continue
+            if distances[i]+w < distances[city]:
+                distances[city] = distances[i]+w
+
+for i in range(1,n+1):
+    for city, w in cities[i]:
+        if distances[i] == float("INF"): continue
+        if distances[i]+w < distances[city]:
+            print(-1);exit()
+
+for i in distances[2:]:
+    print(i if i!=float("INF") else -1)
