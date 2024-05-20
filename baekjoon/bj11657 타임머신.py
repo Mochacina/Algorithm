1bb23c02ab90a5b67db77e7500a7d96bb5ab98d2
mochacina <nayuta5554@gmail.com>
2024-05-20 17:16:23 +0900

    commit solved

--- baekjoon/bj11657 타임머신.py
+++ baekjoon/bj11657 타임머신.py
@@ -1,3 +1,28 @@
+# n,m = map(int,input().split())
+# cities = [[] for _ in range(n+1)]
+# for _ in range(m):
+#     a,b,c = map(int, input().split())
+#     cities[a].append([b,c])
+
+# distances = [float("INF")]*(n+1)
+# distances[1] = 0
+
+# for t in range(n):
+#     for i in range(1,n+1):
+#         for city, w in cities[i]:
+#             if distances[i] == float("INF"): continue
+#             if distances[i]+w < distances[city]:
+#                 distances[city] = distances[i]+w
+
+# for i in range(1,n+1):
+#     for city, w in cities[i]:
+#         if distances[i] == float("INF"): continue
+#         if distances[i]+w < distances[city]:
+#             exit(print(-1))
+
+# for i in distances[2:]:
+#     print(i if i!=float("INF") else -1)
+    
 n,m = map(int,input().split())
 cities = [[] for _ in range(n+1)]
 for _ in range(m):
@@ -7,18 +32,13 @@ for _ in range(m):
 distances = [float("INF")]*(n+1)
 distances[1] = 0
 
-for _ in range(n-1):
+for t in range(n):
     for i in range(1,n+1):
         for city, w in cities[i]:
             if distances[i] == float("INF"): continue
             if distances[i]+w < distances[city]:
+                if t==n-1:exit(print(-1))
                 distances[city] = distances[i]+w
-
-for i in range(1,n+1):
-    for city, w in cities[i]:
-        if distances[i] == float("INF"): continue
-        if distances[i]+w < distances[city]:
-            print(-1);exit()
 
 for i in distances[2:]:
     print(i if i!=float("INF") else -1)
