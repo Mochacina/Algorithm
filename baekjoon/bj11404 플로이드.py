2d67563334fd303042418f2a5989cb9a4cedf46b
Mochacina <nayuta5554@gmail.com>
2024-06-03 02:21:30 +0900

    commit solved

---
+++ baekjoon/bj11404 플로이드.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())
+
+inf = float("inf")
+
+l = [[inf]*(n)for _ in range(n)]
+for i in range(n): l[i][i] = 0
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    a,b=a-1,b-1
+    l[a][b] = min(l[a][b], c)
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            l[j][k] = min(l[j][k], l[i][k]+l[j][i])
+
+for i in l:
+    print(*[(j if j!=inf else 0)for j in i])
