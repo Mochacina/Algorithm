04a8e2c2002a04b2453ceda7170726bf7717c7bb
mochacina <nayuta5554@gmail.com>
2024-07-02 10:40:20 +0900

    commit solved

---
+++ baekjoon/bj13707 합분해.cpp
@@ -0,0 +1,26 @@
+// 이항계수 사용하지 않고 정직하게 DP로 풀이
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int n=0,k=0;
+    cin>>n>>k;
+    int dp[k][n+1];
+    for (int i=0; i<k; i++){
+        dp[i][0] = 1;
+    }
+    for (int j=0; j<=n; j++){
+        dp[0][j] = 1;
+    }
+    
+    for (int i=1; i<k; i++){
+        for (int j=1; j<=n; j++){
+            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            dp[i][j] %= 1000000000;
+        }
+    }
+    cout << dp[k-1][n];
+    return 0;
+}
