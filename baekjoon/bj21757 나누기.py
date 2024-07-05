14898cd1357399723b942a99e7cd1bed0b5f23a2
mochacina <nayuta5554@gmail.com>
2024-07-05 10:24:31 +0900

    commit solved

--- baekjoon/bj21757 나누기.py
+++ baekjoon/bj21757 나누기.py
@@ -1,3 +1,8 @@
-n,s = int(input()),0
-l = [s:=s+i for i in [*map(int,input().split())]]
-if s%4: exit(print(0))
+p=input
+n,s=int(p()),0
+l=[s:=s+i for i in[*map(int,p().split())]]
+d=[1,0,0,0]
+for i in range(n-1):
+ for j in[3,2,1]:
+  if l[i]==s//4*j:d[j]+=d[j-1]
+print([d[3],0][s%4>0])
