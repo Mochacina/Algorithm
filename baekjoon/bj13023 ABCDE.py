6b853b396c44ac61e802a2f604959a7b1d47056f
mochacina <nayuta5554@gmail.com>
2024-07-19 03:13:47 +0900

    commit

--- baekjoon/bj13023 ABCDE.py
+++ baekjoon/bj13023 ABCDE.py
@@ -6,4 +6,18 @@ for _ in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-print(graph)
+
+def dfs(node):
+    nodes = [(node,1)]
+    while nodes:
+        nd, cnt = nodes.pop()
+        if cnt >= 5: exit(print(1))
+        for i in graph[nd]:
+            if not visited[i]:
+                nodes.append((i,cnt+1))
+
+for i in range(n):
+    visited = [0]*n
+    visited[i] = 1
+    dfs(i)
+print(0)
