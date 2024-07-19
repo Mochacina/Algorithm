a26c74440ba315f733c71f4dd7a667e470a09d53
mochacina <nayuta5554@gmail.com>
2024-07-19 15:28:46 +0900

    commit solved

--- baekjoon/bj13023 ABCDE.py
+++ baekjoon/bj13023 ABCDE.py
@@ -7,20 +7,17 @@ for _ in range(m):
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(node):
-    nodes = [(node,1)]
-    while nodes:
-        nd, cnt = nodes.pop()
-        print(nd, cnt)
-        if cnt >= 5: exit(print(1))
-        for i in graph[nd]:
-            if not visited[i]:
-                visited[i] = 1
-                nodes.append((i,cnt+1))
+def dfs(node, cnt, visited):
+    if cnt >= 5: exit(print(1))
+    visited[node] = 1
+    for nghb in graph[node]:
+        if not visited[nghb]:
+            if dfs(nghb, cnt+1, visited): return 1
+    visited[node] = 0
+    return 0    
 
 for i in range(n):
     visited = [0]*n
     visited[i] = 1
-    print(f"dfs: {i}")
-    dfs(i)
+    dfs(i, 1, visited)
 print(0)
