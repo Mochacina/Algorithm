1eacc2a22f351c2336ae15c67cf6330a125b3751
Mochacina <nayuta5554@gmail.com>
2025-08-24 22:08:07 +0900

    commit

--- baekjoon/bj11848 Schools.py
+++ baekjoon/bj11848 Schools.py
@@ -1,10 +1,24 @@
-import heapq
-n,m,s = map(int,input().split())
-hq_m, hq_s = [],[]
-heapq.heapify(hq_m)
-heapq.heapify(hq_s)
-for i in range(n):
-    a,b = map(int,input().split())
-    dif = a-b
-    heapq.heappush(hq_m,(-a,a,dif,i))
-    heapq.heappush(hq_s,(-b,b,-dif,i))
+import sys
+a = [0]*1024
+b = [0]*1024
+dp = [[], []]
+p = False
+q = False
+input = sys.stdin.readline
+k, n, m = map(int, input().split())
+for i in range(1, k + 1):
+    a[i], b[i] = map(int, input().split())
+dp[0] = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+dp[1] = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+p = 1
+q = 0
+for i in range(1, k + 1):
+    p, q = q, p
+    for x in range(0, n + 1):
+        for y in range(0, m + 1):
+            dp[q][x][y] = dp[p][x][y]
+            if x:  
+                dp[q][x][y] = max(dp[q][x][y], dp[p][x - 1][y] + a[i])
+            if y: 
+                dp[q][x][y] = max(dp[q][x][y], dp[p][x][y - 1] + b[i])
+print(dp[q][n][m])
--- baekjoon/bj1600 말이 되고픈 원숭이.py
+++ baekjoon/bj1600 말이 되고픈 원숭이.py
@@ -1,36 +1,36 @@
 from collections import deque
 
+
+# 입력 받기
+K = int(input())  # 말처럼 이동할 수 있는 횟수
+W, H = map(int, input().split())  # 가로, 세로 크기
+grid = []
+for _ in range(H):
+    row = list(map(int, input().split()))
+    grid.append(row)
+
+horse_moves = [
+    (-2, -1), (-2, 1), (-1, -2), (-1, 2),
+    (1, -2), (1, 2), (2, -1), (2, 1)
+]
+
+# 일반 이동 방향 (상하좌우)
+normal_moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+# BFS를 위한 큐 초기화
+# 상태: (행, 열, 남은 말 이동 횟수, 현재까지의 이동 횟수)
+queue = deque([(0, 0, K, 0)])
+
+# 방문 체크 배열 (3차원: 행, 열, 남은 말 이동 횟수)
+visited = [[[False] * (K + 1) for _ in range(W)] for _ in range(H)]
+visited[0][0][K] = True
+
 def solution():
-    # 입력 받기
-    K = int(input())  # 말처럼 이동할 수 있는 횟수
-    W, H = map(int, input().split())  # 가로, 세로 크기
-    grid = []
-    for _ in range(H):
-        row = list(map(int, input().split()))
-        grid.append(row)
-    
-    horse_moves = [
-        (-2, -1), (-2, 1), (-1, -2), (-1, 2),
-        (1, -2), (1, 2), (2, -1), (2, 1)
-    ]
-    
-    # 일반 이동 방향 (상하좌우)
-    normal_moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
-    # BFS를 위한 큐 초기화
-    # 상태: (행, 열, 남은 말 이동 횟수, 현재까지의 이동 횟수)
-    queue = deque([(0, 0, K, 0)])
-    
-    # 방문 체크 배열 (3차원: 행, 열, 남은 말 이동 횟수)
-    visited = [[[False] * (K + 1) for _ in range(W)] for _ in range(H)]
-    visited[0][0][K] = True
-    
     while queue:
         row, col, remaining_horse_moves, moves = queue.popleft()
         
         # 목표 지점 도달 확인
-        if row == H - 1 and col == W - 1:
-            return moves
+        if row == H - 1 and col == W - 1: return moves
         
         # 1. 말처럼 이동 (나이트 이동)
         if remaining_horse_moves > 0:
@@ -58,7 +58,4 @@ def solution():
     # 도달할 수 없는 경우
     return -1
 
-# 메인 실행
-if __name__ == "__main__":
-    result = solution()
-    print(result)
+print(solution())
