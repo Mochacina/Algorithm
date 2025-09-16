0a1e440a90637cf3effa177297ef3335d599008a
Mochacina <nayuta5554@gmail.com>
2025-09-17 01:16:12 +0900

    commit 1786

--- baekjoon/bj1786.py
+++ baekjoon/bj1786.py
@@ -3,26 +3,27 @@ import sys
 T = input()
 P = input()
 
-def get_pi(pattern):
-    m = len(pattern)
+def get_pi(p):
+    m = len(p)
     pi = [0]*m
     j = 0
     for i in range(1,m):
-        while n > 0 and pattern[i] != pattern[j]: n = pi[j-1]
-        if pattern[i] == pattern[j]:
+        while j > 0 and p[i] != p[j]:
+            j = pi[j-1]
+        if p[i] == p[j]:
             j += 1
             pi[i] = j
     return pi
 
-def kmp(text, pattern):
-    pi = get_pi(pattern)
-    n,m = len(text),len(pattern)
+def kmp(t, p):
+    pi = get_pi(p)
+    n,m = len(t),len(p)
     matched = []
     j = 0
     for i in range(n):
-        while j > 0 and text[i] != pattern[j]:
+        while j > 0 and t[i] != p[j]:
             j = pi[j-1]
-        if text[i] == pattern[j]:
+        if t[i] == p[j]:
             if j == m-1:
                 matched.append(i-m+2)
                 j = pi[j]
