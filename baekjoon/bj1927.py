782b130f5587c95736e1b1f19c5e88db3ccf5c12
Mochacina <nayuta5554@gmail.com>
2025-10-05 01:35:13 +0900

    commit 1927

---
+++ baekjoon/bj1927.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+input = sys.stdin.readline
+N = int(input())
+heap = []
+
+for _ in range(N):
+    qst = int(input())
+
+    if qst:
+        heapq.heappush(heap, qst)
+    else:
+        if len(heap):
+            temp = heapq.heappop(heap)
+            print(temp)
+        else:
+            print(0)
