777a19a4ca323c7f6bb5fb351e75fda48f2b399a
Mochacina <nayuta5554@gmail.com>
2025-10-09 05:45:31 +0900

    commit 2805

---
+++ baekjoon/bj2805.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start, end = 0, max(trees)
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    
+    for tree in trees:
+        if tree > mid:
+            total += tree - mid
+    
+    if total >= M:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(result)
