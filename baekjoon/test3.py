d7507d7af595d54e9c17c4a05fdd02466dbd88e4
Mochacina <nayuta5554@gmail.com>
2025-12-16 07:13:09 +0900

    commit

--- baekjoon/test3.py
+++ baekjoon/test3.py
@@ -1,7 +1,3 @@
-n=input()
-l=[*map(int,input().split())]
-y=sum([(1+i//30) for i in l])*10
-m=sum([(1+i//60) for i in l])*15
-if y<m: print(f"Y {y}")
-elif y>m: print(f"M {m}")
-else: print(f"Y M {y}")
+l1 = sum(map(int,input().split()))
+l2 = sum(map(int,input().split()))
+print(max(l1,l2))
