89bc3144ef13390e0ad13910b98f9fe5078b7a9f
Mochacina <nayuta5554@gmail.com>
2026-01-16 03:44:30 +0900

    commit

---
+++ baekjoon/bj28691.py
@@ -0,0 +1,8 @@
+d={
+    'M':'MatKor',
+    'W':'WiCys',
+    'C':'CyKor',
+    'A':'AlKor',
+    '$':'$clear',
+}
+print(d[input()])
--- baekjoon/test3.py
+++ baekjoon/test3.py
@@ -1,3 +1,6 @@
-n=int(input())
-l=[*map(int,input().split())]
-print(1 if l==sorted(l) else 0)
+num=[380,425,450,495,570,590,620,781]
+colors=['Red','Orange','Yellow','Green','Blue','Indigo','Violet']
+m = int(input())
+for i in range(7):
+    if num[i] <= m < num[i+1]:
+        print(colors[::-1][i])
