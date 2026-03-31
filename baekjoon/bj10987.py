cbc90bb368770a0023df827f51794bf2e9fdb244
Mochacina <nayuta5554@gmail.com>
2026-04-01 05:29:36 +0900

    commit

---
+++ baekjoon/bj10987.py
@@ -0,0 +1,2 @@
+n=input()
+print(sum(n.count(i) for i in list(['a','e','i','o','u'])))
---
+++ baekjoon/bj34400.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    n=int(input())+0.5
+    print('ONLINE' if n%25<17 else 'OFFLINE')
