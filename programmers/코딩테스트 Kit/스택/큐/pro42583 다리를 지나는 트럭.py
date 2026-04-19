125d5dcd8f95928cee386979d877f49ce74762bb
Mochacina <nayuta5554@gmail.com>
2026-04-19 22:15:38 +0900

    commit 42583

--- programmers/코딩테스트 Kit/스택/큐/pro42583 다리를 지나는 트럭.py
+++ programmers/코딩테스트 Kit/스택/큐/pro42583 다리를 지나는 트럭.py
@@ -3,7 +3,7 @@ from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     trucks = deque(truck_weights)
-    bridge = deque(list([0]*bridge_length))
+    bridge = deque([0]*bridge_length)
     current_weight = 0
     while trucks or current_weight:
         current_weight -= bridge.popleft()
